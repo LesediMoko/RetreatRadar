e409e69a4205aa9e70935e864d9b5e31db07d626
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-04-06 00:43:40 +0200

    feat: :sparkles: integrated firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"retreat-radar","appId":"1:271718171858:web:c7f6de6bd7b62ff575990b","storageBucket":"retreat-radar.appspot.com","apiKey":"AIzaSyDQNrD11pGYvPxqELyI97areXrB8az2aDA","authDomain":"retreat-radar.firebaseapp.com","messagingSenderId":"271718171858","measurementId":"G-9M0EDD1D1Y"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
