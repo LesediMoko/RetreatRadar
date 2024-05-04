ed42eb035a865de810b011860324e89e496b17a3
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-04 07:25:39 +0200

    feat: :sparkles: added routing for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { SplashComponent } from './modules/splash/components/splash/splash.compo
 import { LoginComponent } from './modules/login/components/login/login.component';
 import { HomeComponent } from './modules/home/components/home/home.component';
 import { authGuardGuard } from './guards/auth-guard.guard';
+import { AppComponent } from './app.component';
+import { ViewComponent } from './modules/common/components/view/view.component';
+import { ViewControllerComponent } from './modules/common/components/view-controller/view-controller.component';
+import { HomeContainerComponent } from './modules/home/components/home-container/home-container.component';
 
 export const routes: Routes = [
   { path: '', component: SplashComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [authGuardGuard] },
+  { path: 'app', component: ViewControllerComponent, children: [{ path: 'home', component: HomeContainerComponent }] },
 ];
