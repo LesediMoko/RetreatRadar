e49b4098084bec8b3590c011edd05fafb2212150
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-04 07:33:55 +0200

    feat: :sparkles: introduced trip side panel to view current trip details

---
+++ src/app/modules/trips/components/trip-panel-container/trip-panel-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-panel-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-panel-container.component.html',
+  styleUrl: './trip-panel-container.component.css'
+})
+export class TripPanelContainerComponent {
+
+}
