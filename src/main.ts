9706b5ad38ea9c9ba0e185b68dc3332bb8c91e92
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-04 07:24:55 +0200

    feat: :sparkles: added store persistence

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { getAllDataFromLocalForage, default as localForage } from 'ngrx-store-persist';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+getAllDataFromLocalForage({ driver: localForage.INDEXEDDB, keys: ['user'] }).then(() => {
+  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+});
