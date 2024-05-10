8dd7e5320ce08cf03d03c627052007e84f160a8a
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-10 15:34:25 +0200

    feat: :sparkles: functionality to for nav bar options (logout user and select currency now added)

--- src/app/modules/common/components/view-controller/view-controller.component.ts
+++ src/app/modules/common/components/view-controller/view-controller.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { UserState } from '../../../../store/Types/states';
+import { Component, OnDestroy } from '@angular/core';
+import { ItineraryItemsState, TripsState, UserState } from '../../../../store/Types/states';
 import { Store } from '@ngrx/store';
 import { selectUserID, selectUserProfile } from '../../../../store/Selectors/user.selector';
 import { ViewComponent } from '../view/view.component';
 import { AsyncPipe } from '@angular/common';
-import { fetchProfile } from '../../../../store/Actions/user.actions';
-import { map } from 'rxjs';
+import { fetchProfile, logout } from '../../../../store/Actions/user.actions';
+import { Subscription, map, pipe, take, takeUntil } from 'rxjs';
+import { Router } from '@angular/router';
+import { clearItemState, fetchConversionRate, updateCurrency } from '../../../../store/Actions/items.actions';
+import { clearTripState } from '../../../../store/Actions/trips.actions';
+import { selectChosenCurrency } from '../../../../store/Selectors/items.selector';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-view-controller',
@@ -14,9 +19,34 @@ import { map } from 'rxjs';
   templateUrl: './view-controller.component.html',
   styleUrl: './view-controller.component.css',
 })
-export class ViewControllerComponent {
-  userProfileInfo$ = this.store.select(selectUserProfile);
-  constructor(private store: Store<UserState>) {
-    this.userProfileInfo$ = this.store.select(selectUserProfile);
+export class ViewControllerComponent implements OnDestroy {
+  userProfileInfo$ = this.userStore.select(selectUserProfile);
+  selectedCurrency$ = this.itemStore.select(selectChosenCurrency);
+  selectedCurrencySubscription$ = new Subscription();
+  constructor(
+    private userStore: Store<UserState>,
+    private tripStore: Store<TripsState>,
+    private itemStore: Store<ItineraryItemsState>,
+    private router: Router,
+  ) {
+    this.userProfileInfo$ = this.userStore.select(selectUserProfile);
+  }
+  logoutUser() {
+    this.itemStore.dispatch(clearItemState());
+    this.tripStore.dispatch(clearTripState());
+    this.userStore.dispatch(logout());
+    this.router.navigate([`/login`]);
+  }
+  chooseCurrency($event: Event) {
+    const currencyOption = ($event.target as HTMLSelectElement).value;
+    const currencyCode = currencyOption.split(' ')[1];
+    this.selectedCurrencySubscription$ = this.selectedCurrency$.pipe(take(1)).subscribe(currency => {
+      console.log(`from: ${currency} to: ${currencyCode}`);
+      this.itemStore.dispatch(fetchConversionRate({ currencyFrom: currency, currencyTo: currencyCode }));
+    });
+    this.itemStore.dispatch(updateCurrency({ currency: currencyCode }));
+  }
+  ngOnDestroy() {
+    this.selectedCurrencySubscription$.unsubscribe();
   }
 }
--- src/app/modules/common/components/view/view.component.ts
+++ src/app/modules/common/components/view/view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ItemFormComponent } from '../../../items/components/item-form/item-form.component';
@@ -6,16 +6,21 @@ import { TripPanelComponent } from '../../../trips/components/trip-panel/trip-pa
 import { ionPersonSharp } from '@ng-icons/ionicons';
 import { tablerCalendarPlus } from '@ng-icons/tabler-icons';
 import { IUser } from '../../types/app-types';
+import { TripPanelContainerComponent } from '../../../trips/components/trip-panel-container/trip-panel-container.component';
+import { ItemFormContainerComponent } from '../../../items/components/item-form-container/item-form-container.component';
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [RouterOutlet, NgIconComponent, TripPanelComponent, ItemFormComponent, RouterLink],
+  imports: [RouterOutlet, NgIconComponent, TripPanelContainerComponent, ItemFormContainerComponent, RouterLink],
   viewProviders: [provideIcons({ ionPersonSharp, tablerCalendarPlus })],
   templateUrl: './view.component.html',
   styleUrl: './view.component.css',
 })
 export class ViewComponent {
   @Input() userProfile: IUser | null = null;
+  @Input() currentCurrency: string = 'ZAR';
+  @Output() onClickLogout = new EventEmitter<void>();
+  @Output() onCurrencyChange = new EventEmitter<Event>();
   constructor() {}
 }
