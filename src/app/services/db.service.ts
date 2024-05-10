ed091f6ce90118558e533c66eb9d2d8b212a1da6
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-10 15:23:32 +0200

    feat: :sparkles: CRUD functionality to edit trip, delete trip and item, add trip and item

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -9,8 +9,8 @@ import {
   signInWithEmailAndPassword,
 } from '@angular/fire/auth';
 import { Observable, Subject, from, fromEvent } from 'rxjs';
-import { IUser } from '../modules/common/types/app-types';
-import { Firestore, doc, setDoc } from '@angular/fire/firestore';
+import { IAddItemForm, IAddTripForm, IItem, ITrip, IUser } from '../modules/common/types/app-types';
+import { Firestore, doc, setDoc, addDoc } from '@angular/fire/firestore';
 import {
   collection,
   getFirestore,
@@ -20,11 +20,14 @@ import {
   getDocs,
   QuerySnapshot,
   documentId,
+  deleteDoc,
 } from '@firebase/firestore';
 import { FirestoreCollections } from '../modules/utils/firestore-collections';
 import { initializeApp } from '@firebase/app';
 import { environment } from '../../environments/environment.development';
 import { debounceTime, distinctUntilChanged, map, switchMap, debounce } from 'rxjs/operators';
+import { deleteItem } from '../store/Actions/items.actions';
+import { deleteTrip } from '../store/Actions/trips.actions';
 
 @Injectable({
   providedIn: 'root',
@@ -55,7 +58,7 @@ export class DbService {
       username: profile.username,
       name: profile.name,
       surname: profile.surname,
-      avater: profile.avatar,
+      avatar: profile.avatar,
     };
     return from(setDoc(doc(this.db, FirestoreCollections.PROFILES, uid), documentInfo));
   }
@@ -94,6 +97,62 @@ export class DbService {
   fetchAllEvents(): Observable<QuerySnapshot<DocumentData>> {
     const eventsRef = collection(this.db, FirestoreCollections.EVENTS);
     const fetchQuery = query(eventsRef);
+    return from(getDocs(fetchQuery));
+  }
+
+  fetchUserTrips(uid: string): Observable<QuerySnapshot<DocumentData>> {
+    const tripsRef = collection(this.db, FirestoreCollections.TRIPS);
+    const fetchQuery = query(tripsRef, where('uid', '==', uid));
+    return from(getDocs(fetchQuery));
+  }
+
+  fetchTripItems(tripID: string | string[]): Observable<QuerySnapshot<DocumentData>> {
+    console.log('fetching items for trip: ', tripID);
+    if (tripID instanceof Array) {
+      const itemsRef = collection(this.db, FirestoreCollections.ITEMS);
+      const fetchQuery = query(itemsRef, where('tripId', 'in', tripID));
+      return from(getDocs(fetchQuery));
+    }
+    const itemsRef = collection(this.db, FirestoreCollections.ITEMS);
+    const fetchQuery = query(itemsRef, where('tripId', '==', tripID));
+    return from(getDocs(fetchQuery));
+  }
+
+  addTrip(trip: IAddTripForm): Observable<ITrip> {
+    return from(
+      addDoc(collection(this.db, FirestoreCollections.TRIPS), trip).then(documentRef => {
+        return { ...trip, id: documentRef.id };
+      }),
+    );
+  }
+
+  addItem(item: IAddItemForm): Observable<IItem> {
+    return from(
+      addDoc(collection(this.db, FirestoreCollections.ITEMS), item).then(documentRef => {
+        return { ...item, id: documentRef.id };
+      }),
+    );
+  }
+
+  editTrip(trip: ITrip): Observable<ITrip> {
+    return from(
+      setDoc(doc(this.db, FirestoreCollections.TRIPS, trip.id), trip).then(() => {
+        return trip;
+      }),
+    );
+  }
+
+  deleteItem(itemId: string): Observable<void> {
+    return from(deleteDoc(doc(this.db, FirestoreCollections.ITEMS, itemId)));
+  }
+
+  deleteTrip(tripId: string): Observable<void> {
+    return from(deleteDoc(doc(this.db, FirestoreCollections.TRIPS, tripId)));
+  }
+
+  fetchUserPreferences(uid: string): Observable<QuerySnapshot<DocumentData>> {
+    const preferencesRef = collection(this.db, FirestoreCollections.PREFERENCES);
+    const fetchQuery = query(preferencesRef, where('userId', '==', uid));
     return from(getDocs(fetchQuery));
   }
 }
