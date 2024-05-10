3392db0ccc40694cecbc57b0917ee2de765fef0d
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-10 15:16:02 +0200

    feat: :sparkles: network call to retrieve the conversion rate given the base and target currencies

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environmentAPI } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { ICurrencyExchangeAPIResponse } from '../modules/common/types/app-types';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ApiService {}
+export class ApiService {
+  latestExchangeRateURL = 'https://api.currencyapi.com/v3/latest';
+  apiKeyHeader: HttpHeaders = new HttpHeaders({
+    apiKey: environmentAPI.currencyApiKey,
+  });
+  constructor(private http: HttpClient) {}
+  getConversionRate(from: string, to: string): Observable<ICurrencyExchangeAPIResponse> {
+    return this.http.get(`${this.latestExchangeRateURL}?base_currency=${from}&currencies=${to}`, {
+      headers: this.apiKeyHeader,
+    }) as Observable<ICurrencyExchangeAPIResponse>;
+  }
+}
