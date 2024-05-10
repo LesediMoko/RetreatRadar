22e4dfeefb5537d153696a75b8bff71a6e4efafb
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-10 15:27:57 +0200

    feat: :sparkles: new selector to select single event (to view details)

--- src/app/store/Selectors/events.selector.ts
+++ src/app/store/Selectors/events.selector.ts
@@ -5,3 +5,5 @@ import { retreatRadarStoreEventsFeatureKey } from '..';
 export const selectEventsFeature = createFeatureSelector<EventsState>(retreatRadarStoreEventsFeatureKey);
 
 export const selectEvents = createSelector(selectEventsFeature, state => state.events);
+
+export const selectSelectedEvent = createSelector(selectEventsFeature, state => state.selectedEvent);
