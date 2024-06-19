c940c9f305195c14ddeebb93c024f12fa0cf6b33
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-06-19 14:45:47 +0200

    feat: :lipstick: added buttons to add or remove events for itinerary list

--- src/app/modules/events/components/event-details-container/event-details-container.component.ts
+++ src/app/modules/events/components/event-details-container/event-details-container.component.ts
@@ -4,7 +4,14 @@ import { Store } from '@ngrx/store';
 import { EventsState, ItineraryItemsState } from '../../../../store/Types/states';
 import { selectSelectedEvent } from '../../../../store/Selectors/events.selector';
 import { AsyncPipe } from '@angular/common';
-import { selectChosenCurrency } from '../../../../store/Selectors/items.selector';
+import {
+  selectChosenCurrency,
+  selectItemIdsBySelectedTripId,
+  selectSelectedTripId,
+} from '../../../../store/Selectors/items.selector';
+import { selectSelectedTrip } from '../../../../store/Selectors/trips.selector';
+import { TripsState } from '../../../../store/Types/states';
+import { IItem } from '../../../common/types/app-types';
 
 @Component({
   selector: 'app-event-details-container',
@@ -16,8 +23,14 @@ import { selectChosenCurrency } from '../../../../store/Selectors/items.selector
 export class EventDetailsContainerComponent {
   eventInfo$ = this.eventStore.select(selectSelectedEvent);
   selectedCurrency$ = this.itemStore.select(selectChosenCurrency);
+  selectedTripId$ = this.tripStore.select(selectSelectedTripId);
+  selectedTripItemsIds$ = this.itemStore.select(selectItemIdsBySelectedTripId);
   constructor(
     private eventStore: Store<EventsState>,
     private itemStore: Store<ItineraryItemsState>,
+    private tripStore: Store<TripsState>,
   ) {}
+  // addEventSubmit($event: IItem) {
+  //   this.itemStore.dispatch(addItem({ item: $event }));
+  // }
 }
--- src/app/modules/events/components/event-details/event-details.component.ts
+++ src/app/modules/events/components/event-details/event-details.component.ts
@@ -1,19 +1,51 @@
-import { Component, Input } from '@angular/core';
-import { IEventItem } from '../../../common/types/app-types';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IAddItemForm, IEventItem, IItem } from '../../../common/types/app-types';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ionCalendarSharp, ionCashOutline, ionLocationSharp, ionTimeOutline } from '@ng-icons/ionicons';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { FSTimestampPipe } from '../../../common/pipes/fs-timestamp.pipe';
+import { Timestamp } from '@angular/fire/firestore';
+import { tablerCalendarMinus, tablerCalendarPlus } from '@ng-icons/tabler-icons';
 
 @Component({
   selector: 'app-event-details',
   standalone: true,
   imports: [NgIconComponent, DatePipe, FSTimestampPipe, CurrencyPipe],
-  viewProviders: [provideIcons({ ionCalendarSharp, ionTimeOutline, ionLocationSharp, ionCashOutline })],
+  viewProviders: [
+    provideIcons({
+      ionCalendarSharp,
+      ionTimeOutline,
+      ionLocationSharp,
+      ionCashOutline,
+      tablerCalendarPlus,
+      tablerCalendarMinus,
+    }),
+  ],
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.css',
 })
 export class EventDetailsComponent {
   @Input() selectedEvent: IEventItem | null = null;
   @Input() selectedCurrency = 'ZAR';
+  @Input() selectedTripId: string | null = null;
+  @Input() selselectedTripItemsIds: string[] | null = null;
+  @Output() addEvent = new EventEmitter<IItem>();
+  @Output() removeEvent = new EventEmitter<string>();
+  onSaveClick(): void {
+    if (!this.selectedEvent || this.selectedEvent.endTime || this.selectedEvent.startTime) return;
+    this.addEvent.emit({
+      id: this.selectedEvent?.id ?? '',
+      name: this.selectedEvent?.name ?? '',
+      startTime: this.selectedEvent?.startTime,
+      endTime: this.selectedEvent?.endTime,
+      tripId: this.selectedTripId ?? '',
+      cost: this.selectedEvent?.cost ?? 0,
+      currency: this.selectedEvent?.currency ?? '',
+      tags: this.selectedEvent?.tags ?? [],
+    });
+  }
+
+  onRemoveClick(): void {
+    this.removeEvent.emit(this.selectedEvent?.id ?? '');
+  }
 }
